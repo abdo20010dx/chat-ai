2e41363a535a390f9cff5653b175c1cf020deff4
abdo20010d0 <a.goma@dafagate.com>
2023-02-07 03:52:44 +0200

    updates

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0', () => { });
 }
 bootstrap();
